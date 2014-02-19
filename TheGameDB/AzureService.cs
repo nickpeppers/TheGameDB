ace703225d7fe2c8f231342ec97d2a6e23f1331f
Nick Peppers <nick.peppers@hitcents.com>
2014-02-18 21:04:37 -0600

    Created repo with test FB login

---
+++ TheGameDB/AzureService.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Microsoft.WindowsAzure.MobileServices;
+
+namespace TheGameDB
+{
+	public static class AzureService
+    {
+        // voting service class to get information from Azure server need url and app key to properly find
+        public static readonly MobileServiceClient MobileService = new MobileServiceClient("https://csintermediateproject.azure-mobile.net/", "zeBmdSnLPMjmgYyBgucUibIGUkwGER72");
+
+        // used mainly to set the current poll for other activities to load
+		//public static Poll Poll { get; set; }
+    }
+}
