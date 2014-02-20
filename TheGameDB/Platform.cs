644ad4a4fc0a474cda70640770c621f87cf14e34
Nick Peppers <nick.peppers@hitcents.com>
2014-02-20 15:41:54 -0600

    Added Platform

---
+++ TheGameDB/Platform.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TheGameDB
+{
+    public class Platform
+    {
+        public int ID { get; set; }
+        public string PlatformTitle { get; set; }
+        public string Overview { get; set; }
+        public string Developer { get; set; }
+        public string Manufacturer { get; set; }
+        public string CPU { get; set; }
+        public string Memory { get; set; }
+        public string Graphics { get; set; }
+        public string Sound { get; set; }
+        public string Display { get; set; }
+        public string Media { get; set; }
+        public string MaxControllers { get; set; }
+        public string Rating { get; set; }
+    }
+}
+
