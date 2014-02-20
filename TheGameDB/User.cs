5e7c7994befafad7a45c2558def38baa89ffc3cb
Nick Peppers <nick.peppers@hitcents.com>
2014-02-20 15:02:09 -0600

    Added the start of the User class

---
+++ TheGameDB/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Policy;
+
+namespace TheGameDB
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Url ProfileUrl { get; set; }
+        public string AccountIdentifier { get; set; }
+    }
+}
+
