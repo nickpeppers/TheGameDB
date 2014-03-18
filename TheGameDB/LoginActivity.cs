a50c7aa02d99c05f7ff2d7fc14a9ad62328a3815
Nick Peppers <nick.peppers@hitcents.com>
2014-03-18 15:23:49 -0500

    Added LoginActivity

--- TheGameDB/LoginActivity.cs
+++ TheGameDB/LoginActivity.cs
@@ -8,12 +8,16 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using TheGameDB.Android;
 
 namespace TheGameDB
 {
     [Activity(Label = "LoginActivity")]			
     public class LoginActivity : Activity
     {
+        private const string AppId = "669004006475405";
+        private const string ExtendedPermissions = "user_about_me,read_stream,publish_stream";
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -24,7 +28,10 @@ namespace TheGameDB
 
             loginButton.Click += (sender, e) => 
             {
-
+                var webAuth = new Intent (this, typeof (FBWebViewAuthActivity));
+                webAuth.PutExtra ("AppId", AppId);
+                webAuth.PutExtra ("ExtendedPermissions", ExtendedPermissions);
+                StartActivityForResult (webAuth, 0);
             };
         }
     }
