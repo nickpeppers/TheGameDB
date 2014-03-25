9905c1d8230776f57faf2bfdbfa7b690e82622dc
Nick Peppers <nick.peppers@hitcents.com>
2014-03-25 08:58:32 -0500

    Working on inserting new User into DB

--- TheGameDB/API/User.cs
+++ TheGameDB/API/User.cs
@@ -5,9 +5,9 @@ namespace TheGameDB
     public class User
     {
         public string UserID { get; set; }
-        public string Email { get; set; }
+        //public string Email { get; set; }
         public string Name { get; set; }
-        public string ProfileUrl { get; set; }
+        //public string ProfileUrl { get; set; }
         public string AccountIdentifier { get; set; }
         public string FacebookToken { get; set; }
     }
