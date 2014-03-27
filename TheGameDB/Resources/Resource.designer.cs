42f123ccfbf17aff71822a62e54139cd5d4ca8d4
Nick Peppers <nick.peppers@hitcents.com>
2014-03-27 14:07:19 -0500

    Moved Images and added login screen image

--- TheGameDB/Resources/Resource.designer.cs
+++ TheGameDB/Resources/Resource.designer.cs
@@ -48,6 +48,9 @@ namespace TheGameDB
 			// aapt resource value: 0x7f020000
 			public const int Icon = 2130837504;
 			
+			// aapt resource value: 0x7f020001
+			public const int THGDBLogo = 2130837505;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
