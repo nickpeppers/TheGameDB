19f715e0b96f59e0a62c80fe7c8dc0364c5d937e
IBSIV <IBSIV31@gmail.com>
2014-04-17 00:27:13 -0500

    XML details completed

--- XMLReader/GamesSolution/GamesSolution/Program.cs
+++ XMLReader/GamesSolution/GamesSolution/Program.cs
@@ -8,42 +8,15 @@ namespace GamesDBGroup
 	class XmlParsingDemo
 	{
 		static void Main(string[] args)
+
 		{
-			XmlDocument doc = new XmlDocument ();
-			doc.Load(@"C:\Users\kylej_000\Documents\GitHub\TheGameDB\XMLReader\getGameList.xml");//test location
+			Game PlaceHolder = new Game ();
+			List<Game> TestModule = new List<Game> ();
+			TestModule = PlaceHolder.GetGameList ();
+			Console.WriteLine (TestModule [1].id+"got here!!!");
+			Console.ReadLine ();
 
-			XmlNodeList nodes = doc.DocumentElement.SelectNodes("/Data/Game");
-
-
-			List<Game> games = new List<Game>();
-
-			foreach (XmlNode node in nodes)
-			{
-				Game game = new Game();
-
-				game.id = node.SelectSingleNode("id").InnerText;
-				game.title = node.SelectSingleNode("GameTitle").InnerText;
-				if (node.SelectSingleNode ("ReleaseDate") != null) {
-					game.releaseDate = node.SelectSingleNode ("ReleaseDate").InnerText;
-				}
-				game.platform=node.SelectSingleNode("Platform").InnerText;
-
-
-				games.Add(game);
-			}
-
-            int x=games.Count;
-            for (int y = 0; y < x; y++)
-            {
-                Console.WriteLine("The ID is: " + games[y].id);
-                Console.WriteLine("The Title of the Game is: " + games[y].title);
-                Console.WriteLine("The Release Date of the Game is: " + games[y].releaseDate);
-                Console.WriteLine("The Platform of the Game is: " + games[y].platform);
-
-                
-            }
-            Console.ReadLine();
-		}
+	
 	}
 
 	class Game
@@ -54,6 +27,45 @@ namespace GamesDBGroup
 		public string platform;
 		public string releaseDate;
 
+		public List<Game> GetGameList(){
+				XmlDocument doc = new XmlDocument ();
+				doc.Load(" http://thegamesdb.net/api/GetGamesList.php?name=halo");//test location
+
+				XmlNodeList nodes = doc.DocumentElement.SelectNodes("/Data/Game");
+
+
+				List<Game> games = new List<Game>();
+
+				foreach (XmlNode node in nodes)
+				{
+					Game game = new Game();
+
+					game.id = node.SelectSingleNode("id").InnerText;
+					game.title = node.SelectSingleNode("GameTitle").InnerText;
+					if (node.SelectSingleNode ("ReleaseDate") != null) {
+						game.releaseDate = node.SelectSingleNode ("ReleaseDate").InnerText;
+					}
+					game.platform=node.SelectSingleNode("Platform").InnerText;
+
+
+					games.Add(game);
+				}
+
+				int x=games.Count;
+				/*for (int y = 0; y < x; y++)
+				{
+					Console.WriteLine("The ID is: " + games[y].id);
+					Console.WriteLine("The Title of the Game is: " + games[y].title);
+					Console.WriteLine("The Release Date of the Game is: " + games[y].releaseDate);
+					Console.WriteLine("The Platform of the Game is: " + games[y].platform);
+
+
+				}*/
+				//Console.ReadLine();
+				return games;
+			}
+		}
 		//place for fanart to list/*IMPORTANT*/
 	}
+
 }
