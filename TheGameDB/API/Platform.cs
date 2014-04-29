10cc49a3074055e0f8d12aa79ae4f907217d5b2c
Nick Peppers <nick.peppers@hitcents.com>
2014-04-29 15:01:38 -0500

    Added get platform

--- TheGameDB/API/Platform.cs
+++ TheGameDB/API/Platform.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Xml;
 
 namespace TheGameDB
 {
     public class Platform
     {
-        public int ID { get; set; }
+        public int PlatformId { get; set; }
         public string PlatformTitle { get; set; }
         public string Overview { get; set; }
         public string Developer { get; set; }
@@ -18,6 +19,33 @@ namespace TheGameDB
         public string MaxControllers { get; set; }
         public string Rating { get; set; }
         //TODO: Need to do get images
+
+        public Game GetPlatform(string PlatformId)
+        {
+            XmlDocument doc = new XmlDocument ();
+            doc.Load("http://thegamesdb.net/api/GetPlatform.php?id=" + PlatformId);
+
+            XmlNodeList nodes = doc.DocumentElement.SelectNodes("/Data/Game");
+
+            Platform platform = new Game();
+
+            foreach (XmlNode node in nodes)
+            {
+                platform.PlatformId = node.SelectSingleNode("id").InnerText;
+                platform.PlatformTitle = node.SelectSingleNode("Platform").InnerText;
+                platform.Overview=node.SelectSingleNode("overview").InnerText;
+                platform.Developer=node.SelectSingleNode("developer").InnerText;
+                platform.Manufacturer = node.SelectSingleNode ("manufacturer").InnerText;
+                platform.CPU = node.SelectSingleNode("cpu").InnerText;
+                platform.Memory = node.SelectSingleNode("memory").InnerText;
+                platform.Graphics = node.SelectSingleNode("graphics").InnerText;
+                platform.Sound = node.SelectSingleNode("sound").InnerText;
+                platform.Media = node.SelectSingleNode("media").InnerText;
+                platform.MaxControllers = node.SelectSingleNode("maxcontrollers").InnerText;
+                platform.Rating = node.SelectSingleNode("Rating").InnerText;
+            }
+            return platform;
+        }
     }
 }
 
